c73282eb666e6fd016bf677a4ee681d9d2953bd1
ArchitGoyal2005 <architgoyal2005@gmail.com>
2024-11-01 22:39:42 +0530

    initialized clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="mx-auto text-center container my-4">
+      <h1 className="text-3xl mb-4">Fancy Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+      </div>
+    </div>
+  );
 }
