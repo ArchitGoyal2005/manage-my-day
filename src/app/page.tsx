a97a902ab245080181f035c22d88ada6934c9693
ArchitGoyal2005 <architgoyal2005@gmail.com>
2024-11-01 23:49:49 +0530

    added sign-in and sign-up routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const { userId } = await auth();
+  if (userId != null) redirect("/events");
+
   return (
     <div className="mx-auto text-center container my-4">
       <h1 className="text-3xl mb-4">Fancy Home Page</h1>
@@ -13,6 +18,7 @@ export default function page() {
         <Button asChild>
           <SignUpButton />
         </Button>
+        <UserButton />
       </div>
     </div>
   );
